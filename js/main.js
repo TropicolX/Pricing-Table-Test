2c19d380ad44ad95d8e07009f65aed30c475c099
TropicolCoder <jacobsbusayo@gmail.com>
2021-09-24 14:42:14 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const toggleDivs = document.querySelectorAll(".toggle");
+
+toggleDivs.forEach((item) => {
+	item.addEventListener("click", (event) => {
+		const keyName = item.closest("tr").id;
+		const dropdown = document.querySelector(`.${keyName}`);
+		dropdown.classList.toggle("hide");
+        const toggleIcon = item.closest("tr").querySelector(".arrow-up");
+		toggleIcon.classList.toggle("rotate");
+	});
+});
+
+/* TO HIDE ELEMENT WHENEVER YOU CLICK OUTSIDE THE ELEMENT
+window.addEventListener("click", (e) => {
+    if (e.target !== toggleDivs[0]) {
+        toggleDivs[0].classList.add("hide")
+    }
+})
+*/
